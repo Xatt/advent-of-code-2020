786b98950c52176d3aa4e35063c6784f217dddaa
Xatt <>
2020-12-13 23:45:26 +0000

    Refactored Day 4 part 1

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -1,12 +1,13 @@
+const REQUIRED_FIELDS: &'static[&'static str] =&["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+
 fn main() {
     let contents = std::fs::read_to_string("data.txt").unwrap();
 
-    let valid = contents.split("\n\n").into_iter().map(|p| p.replace(" ", "\n")).filter(|p| is_valid_passport(p)).count();
+    let valid = contents.split("\n\n").into_iter().filter(|p| is_valid_passport_part_one(p)).count();
     println!("{}", valid);
 }
 
-fn is_valid_passport(passport :&str) -> bool {
-    let data_fields = passport.lines().map(|s| s.split(":").nth(0).unwrap()).collect::<Vec<&str>>();
-    let required_fields:Vec<&str> = vec!["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
-    required_fields.iter().all(|req| data_fields.contains(req))
+fn is_valid_passport_part_one(passport :&str) -> bool {
+    let data_fields = passport.split_whitespace().map(|s| s.split(":").nth(0).unwrap()).collect::<Vec<&str>>();
+    REQUIRED_FIELDS.iter().all(|req| data_fields.contains(req))
 }
